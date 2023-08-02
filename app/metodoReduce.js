bb83bcf2314aea94016c4574cf1f6cd3ab69d346
Nicholas Gerade <nicholasgerade@gmail.com>
2023-08-02 11:15:39 -0300

    Add. método reduce

--- app/metodoReduce.js
+++ app/metodoReduce.js
@@ -1,3 +1,7 @@
+// cria a function 
 function calcularValorTotalDeLivrosDisponiveis(livros) {
     return livros.reduce((acc, livro) => acc + livro.preco, 0).toFixed(2)
+
+    // se não me engano essa função nem foi usada no projeto
+    // foi criado mais para demonstrar o método reduce
 }
