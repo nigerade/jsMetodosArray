3689222ebef1fdeaa8fdf1029e123b2bb8245482
Nicholas Gerade <nicholasgerade@gmail.com>
2023-08-02 12:25:02 -0300

    Add. método Reduce

--- app/metodoReduce.js
+++ app/metodoReduce.js
@@ -1,4 +1,8 @@
 // cria a function 
 function calcularValorTotalDeLivrosDisponiveis(livros) {
     return livros.reduce((acc, livro) => acc + livro.preco, 0).toFixed(2)
+    /* Nesta linha, a função reduce() é aplicada no array de livros para calcular o valor total de 
+    todos os livros disponíveis. O método reduce() acumula o valor total, somando cada preço de livro 
+    (livro.preco) ao acumulador (acc), começando com um valor inicial de 0. O método toFixed(2) é chamado 
+    para formatar o resultado com duas casas decimais.*/
 }
