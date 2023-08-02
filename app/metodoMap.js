171640bdae22da882dae3daa9a7bf4548d1dc899
Nicholas Gerade <nicholasgerade@gmail.com>
2023-08-02 11:22:32 -0300

    Add. método Map

--- app/metodoMap.js
+++ app/metodoMap.js
@@ -1,7 +1,18 @@
+// cria a função
 function aplicarDesconto(livros) {
+    // cria var desconto com valor já definido
     const desconto = 0.3
+
+    /* Aqui é criada uma variável livrosComDesconto para armazenar os livros com os descontos aplicados. 
+        O método map() é utilizado no array de livros recebido como argumento para criar um novo array com as 
+        alterações que serão feitas em cada livro. */
     livrosComDesconto = livros.map(livro => {
-        return {...livro, preco: livro.preco - (livro.preco * desconto)}
+        return { ...livro, preco: livro.preco - (livro.preco * desconto) }
     })
+
+    // retorna os livros com os descontos 
     return livrosComDesconto
+
+    // se não me engano essa função nem foi usada no projeto
+    // foi criado mais para demonstrar o método map
 }
